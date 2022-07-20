ad545b624bf437a9ef16593768a7b245d31826a9
ssanger <blulady@gmail.com>
2022-07-20 12:13:37 -0700

    a few final changes

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,6 +8,8 @@ def index(request):
     # generating counts of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_languages = Language.objects.all().count()
 
     # available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -18,6 +20,9 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_languages': num_languages,
+
     }
 
     return render(request, 'index.html', context=context)
