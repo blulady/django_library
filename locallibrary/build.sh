9645c9fd584875949194ade47a347ec887456006
ssanger <blulady@gmail.com>
2022-12-09 19:34:23 -0800

    adding build and req so can deploy

---
+++ locallibrary/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing python dependencies"
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Running database migrations"
+python manage.py makemigrations
+python manage.py migrate --noinput
+
+echo "Collecting staticfiles"
+python manage.py collectstatic --noinput
+
+python manage.py shell <<EOF
+
+from django.contrib.auth.models import User
+
+User.objects.create_superuser(
+                username = 'sang8',
+                email = 'blulady@gmail.com',
+                password = 'funforeveryone2022!!'
+)
+
+exit()
+
+EOF
